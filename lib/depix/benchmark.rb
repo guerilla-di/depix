8d106949664aa32153bc94a3bb4765763e4bb36f
julik <julik@65967661-7906-0410-b21e-e24373c15fab>
2008-12-21 01:43:34 +0000

    Update bm

--- lib/depix/benchmark.rb
+++ lib/depix/benchmark.rb
@@ -1,14 +1,15 @@
 require File.dirname(__FILE__) + '/../depix'
 
 require 'benchmark'
-puts "Reading DPX header 1000 times"
+
+iter = 10000
+
+puts "Reading DPX header #{iter} times, all data"
 puts Benchmark.measure {
-  (0..4).map do
-    pid=fork
-    if (pid)
-    else
-      10000.times { Depix::Reader.new.from_file(File.dirname(__FILE__)+"/../../test/samples/026_FROM_HERO_TAPE_5-3-1_MOV.0029.dpx") }
-    end
-  end
-  Process.waitall
+  iter.times { Depix.from_file(File.dirname(__FILE__)+"/../../test/samples/026_FROM_HERO_TAPE_5-3-1_MOV.0029.dpx", false) }
+}
+
+puts "Reading DPX header #{iter} times, compact data"
+puts Benchmark.measure {
+  iter.times { Depix.from_file(File.dirname(__FILE__)+"/../../test/samples/026_FROM_HERO_TAPE_5-3-1_MOV.0029.dpx", true) }
 }
